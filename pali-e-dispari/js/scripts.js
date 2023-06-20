cd92e4c9a29eee84fe37e86ead7cebb631169465
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-06-20 17:20:55 +0200

    Add numero casuale computer

--- pali-e-dispari/js/scripts.js
+++ pali-e-dispari/js/scripts.js
@@ -7,9 +7,13 @@ const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5:"));
 console.log("Numero utente: " + userNumber);
 
 
-// Funzione per verificare se un numero è pari 
-  function isEven(number) {
-    return number % 2 === 0;
+
+// Genera un numero casuale per il computer
+const computerNumber = getRandomNumber(1, 5);
+console.log("Numero generato dal computer: " + computerNumber);
+
+// Funzione per generare un numero casuale
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;   
   }
 
-  
