8599225f38d6810d63b29c3f405d420b93d94a9e
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-06-20 18:01:24 +0200

    esercizio terminato

--- pali-e-dispari/js/scripts.js
+++ pali-e-dispari/js/scripts.js
@@ -16,50 +16,43 @@ function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;   
   }
 
-//commit 3 add funzione per somma, pari e dispari 
 
 // Funzione per verificare se un numero è pari 
   function isEven(number) {
     return number % 2 === 0;
   }
 
-  // Calcola la somma dei due numeri
+// Calcola la somma dei due numeri
   const sum = userNumber + computerNumber;
   console.log("La somma dei due numeri: " + sum);
 
 // Verifica se la somma è pari o dispari
   const isSumEven = isEven(sum);
-  console.log("La somma dei numeri è pari: " + isSumEven);
+  console.log("La somma dei numeri è pari");
 
-
-
-// commit 4 add condizione per il vincitore
 
 // Variabile per il vincitore
-  let winner;
+let winner;
 
-  if ((isSumEven && userChoice.toLowerCase() === "pari") || (!isSumEven && userChoice.toLowerCase() === "dispari")) {
-    winner = "Utente";
-  } else {
-    winner = "Computer";
-  }
-  console.log("Il vincitore è: " + winner);
-
-
-// commit 5 add stampa a web e termine esercizio
+if ((isSumEven && userChoice.toLowerCase() === "pari") || (!isSumEven && userChoice.toLowerCase() === "dispari")) {
+  winner = "Utente";
+} else {
+  winner = "Computer";
+}
+console.log("Il vincitore è: " + winner);
 
 
 // Accesso all'elemento HTML per visualizzare l'output
-  const outputElement = document.getElementById("output");
+const outputElement = document.getElementById("output");
+
+outputElement.innerHTML += "<h1>Pari o Dispari</h1>";
+outputElement.innerHTML += "<h3>Scegli Pari o Dispari</h3> <br>"
+outputElement.innerHTML += "<u>Risposta utente:</u> " + userChoice + "<br><br>";
+outputElement.innerHTML += "<h3>Inserisci un numero da 1 a 5</h3> <br>";
+outputElement.innerHTML += "<u>Risposta utente:</u> " + userNumber + "<br><br>";
+outputElement.innerHTML += "<h3>Numero generato dal computer:</h3> " + computerNumber + "<br><br>";
+outputElement.innerHTML += "<h3>La somma dei due numeri:</h3> " + sum + "<br></br>";
+outputElement.innerHTML += "<h2>La somma dei numeri è "  + (isSumEven ? "PARI</h2>" : "DISPARI") + "<br><br>";
+outputElement.innerHTML += "<h1>Il vincitore è: " + winner + "</h1><br>";
 
 
-  outputElement.innerHTML += "<h1>Pari o Dispari</h1>";
-  outputElement.innerHTML += "<h3>Scegli Pari o Dispari</h3> <br>"
-  outputElement.innerHTML += "<u>Risposta utente:</u> " + userChoice + "<br><br>";
-  outputElement.innerHTML += "<h3>Inserisci un numero da 1 a 5</h3> <br>";
-  outputElement.innerHTML += "<u>Risposta utente:</u> " + userNumber + "<br><br>";
-  outputElement.innerHTML += "<h3>Numero generato dal computer:</h3> " + computerNumber + "<br><br>";
-  outputElement.innerHTML += "<h3>La somma dei due numeri:</h3> " + sum + "<br></br>";
-  outputElement.innerHTML += "<h2>La somma dei numeri è "  + (isSumEven ? "PARI</h2>" : "DISPARI") + "<br><br>";
-  outputElement.innerHTML += "<h1>Il vincitore è: " + winner + "</h1><br>";
-  
