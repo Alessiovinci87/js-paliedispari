ef622b89290b5ef9739b0646d521d116c5b8b172
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-06-20 16:02:25 +0200

    Esercizio palindromi terminato

--- palindromi/js/scripts.js
+++ palindromi/js/scripts.js
@@ -1,2 +1,17 @@
+function isPalindrome(word) {
 
-  
+    word = word.toLowerCase();
+
+    const reversedWord = word.split('').reverse().join('');
+
+    return word === reversedWord;
+}
+
+
+const userInput = prompt("Inserisci una parola:");
+const resultMessage = isPalindrome(userInput)
+    ? "La parola inserita è palindroma."
+    : "La parola inserita non è palindroma.";
+
+console.log(resultMessage);
+document.getElementById("result").innerHTML = `Parola inserita: ${userInput}<br>${resultMessage}`;
